c712fb9ae44550a51d1ec83707c8bc84ee437fe7
Maciej Rowiński <mrowinski@it-dev.pl>
2016-12-05 13:36:24 +0100

    table changed to list

--- test1.js
+++ test1.js
@@ -3,20 +3,9 @@
  */
 var data=[];
 $.getJSON('http://jsonplaceholder.typicode.com/posts',function(data){
-    for(var i=0;i<data.length+1;i++)
+    for(var i=0;i<data.length;i++)
     {
-        var table = document.getElementById("myTable");
-        var row = table.insertRow(i);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        if(i==0) {
-            cell1.innerHTML = "TITLE";
-            cell2.innerHTML = "BODY";
-
-        }
-        else{
-            cell1.innerHTML =data[i].title;
-            cell2.innerHTML =data[i].body;
-        }
+        $("#new").append("<li> Title: "+data[i].title+"</li>");
+        $("#new").append("<li> Body: "+data[i].body+"</li><br></br>");
     }
 });
